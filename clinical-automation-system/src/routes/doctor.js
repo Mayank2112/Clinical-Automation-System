c3f22727521229a6fd423c887e704c81e8c9e0ff
Mayank Parikh <mparikh@techracers.io>
2020-06-05 22:05:50 +0530

    feat : Create routes for appointment requests

--- clinical-automation-system/src/routes/doctor.js
+++ clinical-automation-system/src/routes/doctor.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import { resetLoginFailure } from '../middlewares/user';
-import { redirectDashboard, redirectDetails, addCredentials } from '../controllers/doctor';
 import { checkCredentials } from '../middlewares/doctor';
 import invalidRoutes from './invalidRoutes';
+import {
+  redirectDashboard,
+  redirectDetails,
+  addCredentials,
+  sendAppointmentRequestList,
+  configureAppointmentRequest,
+  sendAppointmentList
+} from '../controllers/doctor';
 
 const router = Router();
 
@@ -12,6 +19,12 @@ router.get('/dashboard', resetLoginFailure, redirectDashboard);
 router.get('/details', redirectDetails);
 
 router.post('/details', checkCredentials, addCredentials, redirectDetails);
+
+router.get('/appointmentRequest', sendAppointmentRequestList);
+
+router.post('/appointmentRequest', configureAppointmentRequest);
+
+router.get('/appointment', sendAppointmentList);
 
 // Invalid routes or methods
 router.all('/', invalidRoutes);
--- clinical-automation-system/src/routes/patient.js
+++ clinical-automation-system/src/routes/patient.js
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import { resetLoginFailure } from '../middlewares/user';
-import { redirectDashboard } from '../controllers/patient';
+import { checkDoctorAvailability, checkAppointmentData } from '../middlewares/appointment';
 import invalidRoutes from './invalidRoutes';
+import {
+  redirectDashboard,
+  sendDoctorList,
+  makeAppointmentRequest,
+  sendAppointmentList
+} from '../controllers/patient';
 
 const router = Router();
 
 // Dashborad route to access dashboard after login
 router.get('/dashboard', resetLoginFailure, redirectDashboard);
+
+router.get('/appointmentRequest', sendDoctorList);
+
+router.post('/appointmentRequest', checkAppointmentData, checkDoctorAvailability, makeAppointmentRequest);
+
+router.get('/appointment', sendAppointmentList);
 
 // Invalid routes or methods
 router.all('/', invalidRoutes);
