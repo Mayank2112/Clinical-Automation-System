3ff7b217b09ceb08d90caf86a9e6d62738766567
Mayank Parikh <mparikh@techracers.io>
2020-06-17 11:12:50 +0530

    test : Add helper functions for testing

---
+++ clinical-automation-system/test/helpers/admin.js
@@ -0,0 +1,38 @@
+import db from '../../src/models';
+import { v4 } from 'uuid';
+import { hashSync } from 'bcryptjs';
+
+const { Admin, sequelize } = db;
+
+const createAdmin = admin => sequelize.authenticate()
+  .then(() => Admin.sync({ force: false })
+    .then(() => Admin.create({
+      id: v4(),
+      name: admin.name,
+      gender: admin.gender,
+      address: admin.address,
+      mobileNumber: admin.mobileNumber,
+      email: admin.email,
+      password: hashSync(admin.password, 10)
+    })))
+  .catch(err => {
+    console.error(err);
+    return false;
+  });
+
+const deleteAdmin = adminEmail => sequelize.authenticate()
+  .then(() => Admin.sync({ force: false })
+    .then(() => Admin.destroy({
+      where: {
+        email: adminEmail
+      }
+    })))
+  .catch(err => {
+    console.error(err);
+    return false;
+  });
+
+export {
+  createAdmin,
+  deleteAdmin
+};
